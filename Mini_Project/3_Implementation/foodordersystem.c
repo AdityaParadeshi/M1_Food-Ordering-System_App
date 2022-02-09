651cc3f962f7dcc7d120a8d79dd3eafa62ac318f
AdityaParadeshi <adityaparadeshi91@gmail.com>
2022-02-09 14:07:29 +0530

    changes in code

--- Mini_Project/3_Implementation/foodordersystem.c
+++ Mini_Project/3_Implementation/foodordersystem.c
@@ -15,250 +15,250 @@
 
 struct order{
     char name[50];
-    int order_number;
+    int order_no;
     int number_of_food;
     int cost;
 }p[10000];
-int order_count = 0, served_count = 0, waiting_now = 0;
+int order_cnt = 0, served_cnt = 0, waiting_now = 0;
 
 int order ()
 {
-    int n, in, qu;
-    printf ("please enter your name: ");
-    gets (p[order_count].name);
-    p[order_count].order_number = 10000+order_count;
+    int num, inn, qunt;
+    printf (" enter your name: ");
+    gets (p[order_cnt].name);
+    p[order_cnt].order_no = 10000+order_cnt;
     items();
-    p[order_count].number_of_food = n;
-    p[order_count].cost = 0;
-    p[order_count].number_of_food = 0;
+    p[order_cnt].number_of_food = num;
+    p[order_cnt].cost = 0;
+    p[order_cnt].number_of_food = 0;
     
     level:
-    printf ("How many items you want to order? ");
-    scanf ("%d", &n);
+    printf (" enter number of items you want to order? ");
+    scanf ("%d", &num);
 
     getchar ();
-    while (n--){
-        printf ("\nEnter chosen item code\n");
-        scanf ("%d", &in);
+    while (num--){
+        printf ("\nEnter item code you want to order\n");
+        scanf ("%d", &inn);
         getchar();
-        switch(in){
+        switch(inn){
     case 11 : {
-        printf ("Enter the size of burger: 1. 200gm  2. 300gm\n");
+        printf ("Enter the size of cake: 1. 200gm  2. 300gm\n");
         int i;
         scanf ("%d", &i);
         getchar();
 
-        printf ("Please enter the quantity: ");
-        scanf ("%d", &qu);
+        printf (" enter the quantity: ");
+        scanf ("%d", &qunt);
         getchar();
 
-        p[order_count].number_of_food +=qu;
+        p[order_cnt].number_of_food +=qunt;
 
-        if (i==1) p[order_count].cost += (130*qu);
+        if (i==1) p[order_cnt].cost += (130*qunt);
 
-        else p[order_count].cost += (180*qu);
+        else p[order_cnt].cost += (180*qunt);
         break;
     }
     case 12 :{
-        printf ("Enter the size of Chicken Pizza: 1. 12inch  2. 14inch  3. 16inch\n");
+        printf ("Enter the size of pizzasandwitch: 1. 10inch  2. 13inch  3. 17inch\n");
         int i;
         scanf ("%d", &i);
         getchar();
 
-        printf ("Please enter the quantity: ");
-        scanf ("%d", &qu);
+        printf (" enter the quantity: ");
+        scanf ("%d", &qunt);
         getchar();
 
-        p[order_count].number_of_food +=qu;
+        p[order_cnt].number_of_food +=qunt;
 
-        if (i==1) p[order_count].cost += (750*qu);
+        if (i==1) p[order_cnt].cost += (750*qunt);
 
-        else if (i==2) p[order_count].cost += (975*qu);
+        else if (i==2) p[order_cnt].cost += (975*qunt);
 
-        else  p[order_count].cost += (1250*qu);
+        else  p[order_cnt].cost += (1250*qunt);
 
         break;
     }
     case 13 :{
-        printf ("Enter the amount of chicken: 1. 1 pcs  2. 4 pcs  3. 10 pcs\n");
+        printf ("Enter the amount of Potato fries: 1. 1 pcs  2. 4 pcs  3. 10 pcs\n");
         int i;
         scanf ("%d", &i);
         getchar();
 
-        printf ("Please enter the quantity: ");
-        scanf ("%d", &qu);
+        printf (" enter the quantity: ");
+        scanf ("%d", &qunt);
         getchar();
 
-        p[order_count].number_of_food +=qu;
+        p[order_cnt].number_of_food +=qunt;
 
-        if (i==1) p[order_count].cost += (85*qu);
+        if (i==1) p[order_cnt].cost += (85*qunt);
 
-        else if (i==2) p[order_count].cost += (320*qu);
+        else if (i==2) p[order_cnt].cost += (320*qunt);
 
-        else  p[order_count].cost += (790*qu);
+        else  p[order_cnt].cost += (790*qunt);
 
         break;
     }
     case 14 :{
-        printf ("Enter the size of grilled chicken: 1. quarter  2. half  3. full\n");
+        printf ("Enter the size of veg  rice: 1. quarter  2. half  3. full\n");
         int i;
         scanf ("%d", &i);
         getchar();
 
-        printf ("Please enter the quantity: ");
-        scanf ("%d", &qu);
+        printf (" enter the quantity: ");
+        scanf ("%d", &qunt);
         getchar();
 
-        p[order_count].number_of_food +=qu;
+        p[order_cnt].number_of_food +=qunt;
 
-        if (i==1) p[order_count].cost += (70*qu);
+        if (i==1) p[order_cnt].cost += (70*qunt);
 
-        else if (i==2) p[order_count].cost += (140*qu);
+        else if (i==2) p[order_cnt].cost += (140*qunt);
 
-        else  p[order_count].cost += (250*qu);
+        else  p[order_cnt].cost += (250*qunt);
 
         break;
     }
     case 15 :{
-        printf ("Enter the size of Noodles: 1. half  2. full\n");
+        printf ("Enter the size of Biryani: 1. half  2. full\n");
         int i;
         scanf ("%d", &i);
         getchar();
 
-        printf ("Please enter the quantity: ");
-        scanf ("%d", &qu);
+        printf (" enter the quantity: ");
+        scanf ("%d", &qunt);
         getchar();
 
-        p[order_count].number_of_food +=qu;
+        p[order_cnt].number_of_food +=qunt;
 
-        if (i==1) p[order_count].cost += (130*qu);
+        if (i==1) p[order_cnt].cost += (130*qunt);
 
-        else p[order_count].cost += (250*qu);
+        else p[order_cnt].cost += (250*qunt);
 
         break;
     }
     case 16 :{
-        printf ("Enter the size of pasta: 1. half  2. full\n");
+        printf ("Enter the size of masalabhel: 1. half  2. full\n");
         int i;
         scanf ("%d", &i);
         getchar();
 
-        printf ("Please enter the quantity: ");
-        scanf ("%d", &qu);
+        printf (" enter the quantity: ");
+        scanf ("%d", &qunt);
         getchar();
 
-        p[order_count].number_of_food +=qu;
+        p[order_cnt].number_of_food +=qunt;
 
-        if (i==1) p[order_count].cost += (120*qu);
+        if (i==1) p[order_cnt].cost += (120*qunt);
 
-        else p[order_count].cost += (240*qu);
+        else p[order_cnt].cost += (240*qunt);
 
         break;
     }
     case 17 : {
-        printf ("250 ml of OREO SHAKE\n");
+        printf ("250 ml of Milk SHAKE\n");
 
-        printf ("Please enter the quantity: ");
-        scanf ("%d", &qu);
+        printf (" enter the quantity: ");
+        scanf ("%d", &qunt);
         getchar();
 
-        p[order_count].number_of_food +=qu;
-        p[order_count].cost += (180*qu);
+        p[order_cnt].number_of_food +=qunt;
+        p[order_cnt].cost += (180*qunt);
         break;
     }
     case 18 : {
-        printf ("250 ml of Cold Coffee\n");
+        printf ("250 ml of Apple Juice\n");
 
-        printf ("Please enter the quantity: ");
-        scanf ("%d", &qu);
+        printf (" enter the quantity: ");
+        scanf ("%d", &qunt);
         getchar();
 
-        p[order_count].number_of_food +=qu;
-        p[order_count].cost += (65*qu);
+        p[order_cnt].number_of_food +=qunt;
+        p[order_cnt].cost += (65*qunt);
         break;
     }
     case 19 : {
-        printf ("250 ml of Cappuccino\n");
+        printf ("250 ml of Coffee\n");
 
-        printf ("Please enter the quantity: ");
-        scanf ("%d", &qu);
+        printf (" enter the quantity: ");
+        scanf ("%d", &qunt);
         getchar();
 
-        p[order_count].number_of_food +=qu;
-        p[order_count].cost += (70*qu);
+        p[order_cnt].number_of_food +=qunt;
+        p[order_cnt].cost += (70*qunt);
         break;
     }
     case 20 :{
-        printf ("Enter the size of Coke: 1. 250ml  2. 500ml\n");
+        printf ("Enter the size of Sprite: 1. 250ml  2. 500ml\n");
         int i;
         scanf ("%d", &i);
         getchar();
 
-        printf ("Please enter the quantity: ");
-        scanf ("%d", &qu);
+        printf (" enter the quantity: ");
+        scanf ("%d", &qunt);
         getchar();
 
-        p[order_count].number_of_food +=qu;
+        p[order_cnt].number_of_food +=qunt;
 
-        if (i==1) p[order_count].cost += (20*qu);
+        if (i==1) p[order_cnt].cost += (20*qunt);
 
-        else p[order_count].cost += (30*qu);
+        else p[order_cnt].cost += (30*qunt);
         break;
     }
     default : {
-        printf ("Invalid selection try again\n");
-        n++;
+        printf ("Invalid input try again\n");
+        num++;
     }
         }
     }
     char temp;
-    printf ("Do you want to order anything else?(y/n)\n ");
+    printf ("Do you want to order any item else?(yes/no)\n ");
     scanf("%c", &temp);
     if (temp == 'y' || temp == 'Y')
         goto level;
-    printf ("\nNumber of food ordered %d\n", p[order_count].number_of_food);
-    printf ("\nThank you %s for your order. Your bill is %drs.\nPlease wait while we prepare the food.\n\n", p[order_count].name, p[order_count].cost);
-    order_count ++;
+    printf ("\nNumber of food ordered %d\n", p[order_cnt].number_of_food);
+    printf ("\nThank you %s for your order. Your bill is %drs.\nPlease wait while we prepare the food.\n\n", p[order_cnt].name, p[order_cnt].cost);
+    order_cnt ++;
 }
 int receive ()
 {
-    if (order_count==0){
-        printf ("Please order first\n");
+    if (order_cnt==0){
+        printf ("enter order first\n");
         return;
     }
 
-    else if (served_count == order_count) {
+    else if (served_cnt == order_cnt) {
         printf ("All order served\n");
         return;
     }
 
-    printf ("Order no. %d by %s is ready\n", p[served_count].order_number, p[served_count].name);
+    printf ("Order no. %d by %s is ready\n", p[served_cnt].order_no, p[served_cnt].name);
     printf ("Enjoy your meal\n\n");
-    served_count++;
+    served_cnt++;
 }
 int display ()
 {
-    printf ("Total order taken: %d\n", (order_count));
-    printf ("Total number of order served %d\n", served_count);
-    printf ("Number of Currently waiting to be served: %d\n", (order_count-served_count));
-    printf ("Currently preparing food for order no. %d\n\n", p[served_count].order_number);
+    printf ("Total order taken: %d\n", (order_cnt));
+    printf ("Total number of order served %d\n", served_cnt);
+    printf ("Number of Currently waiting to be served: %d\n", (order_cnt-served_cnt));
+    printf ("Currently preparing food for order no. %d\n\n", p[served_cnt].order_no);
 }
 void items ()
 {
-    printf("..................................MENU....................................\n");
+    printf("..................................MENU...................................\n");
 
     printf("Item Code#   Description-------------------Size--------------------Price(rs)\n");
 
-    printf("[11]---------Burger---------------------200/300gm------------------130/180\n");
-    printf("[12]---------Chicken Pizza--------------12/14/16inch---------------750/975/1250\n");
-    printf("[13]---------Fried Chicken--------------1/4/10 pcs-----------------85/320/790\n");
-    printf("[14]---------Grilled Chicken------------quarter/half/full----------70/140/250\n");
-    printf("[15]---------Noodles--------------------Half/Full------------------130/250\n");
-    printf("[16]---------Pasta----------------------Half/Full------------------120/240\n");
-    printf("[17]---------Oreo Shake-----------------250ml----------------------180\n");
-    printf("[18]---------Cold Coffee----------------250ml----------------------65\n");
-    printf("[19]---------Cappuccino-----------------250ml----------------------70\n");
-    printf("[20]---------Coke-----------------------250/500ml------------------20/30\n\n");
+    printf("[11]---------Cake---------------------200/300gm------------------130/180\n");
+    printf("[12]---------pizzasandwitch-------------12/14/16inch---------------750/975/1250\n");
+    printf("[13]---------Potato Fries----------1/4/10 pcs-----------------85/320/790\n");
+    printf("[14]---------veg rice-----------quarter/half/full----------70/140/250\n");
+    printf("[15]---------Biryani------------------Half/Full------------------130/250\n");
+    printf("[16]--------- Masalabhel------------Half/Full------------------120/240\n");
+    printf("[17]---------Milk Shake---------------250ml----------------------180\n");
+    printf("[18]---------Apple Juice---------------250ml----------------------65\n");
+    printf("[19]---------coffee--------------------250ml----------------------70\n");
+    printf("[20]---------Sprite--------------------250/500ml------------------20/30\n\n");
 
 }
 int main ()
@@ -285,7 +285,7 @@ int main ()
             }
             case 4 : {
                 system ("COLOR 2");
-                printf ("We are closed come by again\n\n");
+                printf ("Cafe is closed come  again\n\n");
 
             
              
